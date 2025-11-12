f5da699ae1e0b74b7bf5ec94177ee8a6619240a8
Jakob Ohlsén S. <jowlen@hotmail.se>
2025-11-12 09:34:41 +0100

    Lägger upp en hel massa i main och kommer skapa olika branches, del 1 o del2, med enbart relevanta delar, från main

--- src/moduls.py
+++ src/moduls.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+np.random.seed(42)
 
 def showing_standard_info(df: pd.DataFrame) -> pd.DataFrame:
     list_of_some_numeric_categories = ["age", "weight", "height", "systolic_bp", "cholesterol"]
@@ -36,4 +37,63 @@ def healthy_vs_diseased_info(df):
     df2 = df_healthy.groupby(["smoker", "sex"])["systolic_bp"].mean()
     
     return df1, df2
+
+def healthy_vs_diseased(df):
+    mask_disease = df["disease"] > 0
+    mask_healthy = df["disease"] < 1
+
+    df_disease = df[mask_disease]
+    df_healthy = df[mask_healthy]
+
+    return df_disease, df_healthy
+
+def frequency_of_diseased(df):
+    tu = healthy_vs_diseased(df)
+    tu[0].value_counts().sum()
+    tu[1].value_counts().sum()
+
+    return round((tu[0].value_counts().sum() / (tu[0].value_counts().sum() + tu[1].value_counts().sum())), 3)
+
+def creating_a_random_data_set_with_disease_frequency(df):
+    outcome = [0, 1]
+    disease_probability = [1 - frequency_of_diseased(df), frequency_of_diseased(df)]
+    n = 1000
+    data = np.random.choice(a=outcome, size=n, p=disease_probability)
+    return data
+
+def ci_mean_norma(x, confidence=0.95):
+
+    x = np.asarray(x, dtype=float)
+    mean_x = float(np.mean(x))
+    s = float(np.std(x, ddof=1))
+    n = len(x)
+
+    z_critical = 1.96
+
+    margin_of_error = z_critical * (s / np.sqrt(n))
+
+    lo, hi = mean_x - margin_of_error, mean_x + margin_of_error
+
+    return lo, hi, mean_x, s, n
+
+def ci_mean_bootstrap(x, B=10000, confidence=0.95):
+    x = np.asarray(x, dtype=float)
+    n = len(x)
+    boot_means = np.empty(B)
+    for i in range(B):
+        boot_sample = np.random.choice(x, size=n, replace=True)
+        boot_means[i] = np.mean(boot_sample)
+
+    alpha = (1 - confidence) / 2
+    blo, bhi = np.percentile(boot_means, [100*alpha, 100*(1 - alpha)])
+    return float(blo), float(bhi), float(np.mean(x))
+    
+def looking_for_them_smokers(df: pd.DataFrame):
+    mask_smokers = df["smoker"] == "Yes"
+    mask_non_smokers = df["smoker"] == "No"
+
+    df_smokers = df[mask_smokers]
+    df_non_smokers = df[mask_non_smokers]
+
+    return df_smokers, df_non_smokers
 
--- src/viz.py
+++ src/viz.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from src.moduls import *
 
+
 def plot_gender_weight_difference(df: pd.DataFrame):
     fig, ax = plt.subplots(figsize=(8,5))
     df.boxplot(column="weight", by="sex", ax=ax)
@@ -11,20 +12,21 @@ def plot_gender_weight_difference(df: pd.DataFrame):
     plt.tight_layout()
     plt.show
 
+
 def plot_disease_vs_healthy(df1, df2: pd.DataFrame):
     fig, axes = plt.subplots(1, 2, figsize=(10,8), sharey=False)
 
     df1.plot(kind="bar", ax=axes[0], legend=False)
     axes[0].set_title("Non disease-people")
     axes[0].set_xlabel("")
-    axes[0].set_ylabel("Blood pressure")
+    axes[0].set_ylabel("Mean - Blood pressure")
     axes[0].tick_params(axis="x", rotation=0)
     axes[0].set_ylim(140,160)
 
     df2.plot(kind="bar", ax=axes[1], legend=False)
     axes[1].set_title("Disease-people")
     axes[1].set_xlabel("")
-    axes[1].set_ylabel("Blood pressure")
+    axes[1].set_ylabel("Mean - Blood pressure")
     axes[1].tick_params(axis="x", rotation=0)
     axes[1].set_ylim(140,160)
 
@@ -32,3 +34,55 @@ def plot_disease_vs_healthy(df1, df2: pd.DataFrame):
     plt.tight_layout()
     plt.show()
     
+
+def plot_comparing_age_with_cholesterol(df1, df2: pd.DataFrame):
+    fig, axes = plt.subplots(1, 2, figsize=(10,7))
+
+    df1.plot(kind="scatter", ax=axes[0], legend=False, x="age", y="cholesterol", color="red" )
+    axes[0].set_title("Comparing age with cholesterol - disease")
+    axes[0].set_ylabel("Cholesterol")
+    axes[0].tick_params(axis="x", rotation=0)
+    axes[0].set_ylim(2,8)
+    axes[0].set_xlim(0,90)
+
+    df2.plot(kind="scatter", ax=axes[1], legend=False, x="age", y="cholesterol")
+    axes[1].set_title("Comparing age with cholesterol - healthy")
+    axes[1].set_ylabel("Cholesterol")
+    axes[1].tick_params(axis="x", rotation=0)
+    axes[1].set_ylim(2,8)
+    axes[1].set_xlim(0,90)
+
+    plt.tight_layout()
+    plt.show()
+
+def just_a_basic_plot(df: pd.DataFrame, x_label_and_category: str, y_label_and_category: str):
+    fig, ax = plt.subplot(figsize=(10,8))
+    df.bar(x_label_and_category, y_label_and_category, color="seagreen")
+    ax.set_title("")
+    ax.set_xlabel(x_label_and_category)
+    ax.set_ylabel(y_label_and_category)
+    plt.tight_layout()
+    plt.show()
+
+def ci_mean_and_ci_bootstrap(df: pd.DataFrame):
+    lo, hi, x_mean, s, n = ci_mean_norma(df)
+    blo, bhi, b_mean = ci_mean_bootstrap(df)
+    bm = np.array([np.mean(np.random.choice(df, size=len(df), replace=True)) for _ in range(1000)])
+    fig, axes = plt.subplots(1, 2, figsize=(10,8))
+
+    axes[0].errorbar([0], [x_mean], yerr=[[x_mean - lo], [hi - x_mean]], fmt="o", capsize=6)
+    axes[0].errorbar([1], [b_mean], yerr=[[b_mean - blo], [bhi - b_mean]], fmt="o", capsize=6, color="r", label="Bootstrap CI")
+    axes[0].set_xticks([])
+    axes[0].grid(True, axis="y")
+    axes[0].legend()
+    axes[0].set_title("95% konfidensintervall med normalapproximation")
+
+    axes[1].hist(bm, bins=30, edgecolor="black")
+    axes[1].axvline(x_mean, color="tab:green", linestyle="--", label="Stickprovsmedel")
+    axes[1].axvline(np.percentile(bm, 2.5), color="tab:red", linestyle="--", label="2,5%")
+    axes[1].axvline(np.percentile(bm, 97.5), color="tab:red", linestyle="--", label="97,5%")
+    axes[1].set_title("Bootstrap fördelninga av medel + 95 procent -intervall")
+    axes[1].set_xlabel("Resamplat medel")
+    axes[1].set_ylabel("Antal")
+    axes[1].grid(True, axis="y")
+
